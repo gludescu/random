8e9f3c987bc9bd85b4c26f99595ecbc5e89b1cb6
lduu <98633536+lduu@users.noreply.github.com>
2023-04-15 13:33:59 +0300

    text from images to document

---
+++ images_to_docx_romanian.py
@@ -0,0 +1,34 @@
+import os
+import pytesseract
+from PIL import Image
+from docx import Document
+
+def get_image_files():
+    image_extensions = (".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff")
+    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.lower().endswith(image_extensions)]
+    files.sort()
+    return files
+
+def get_text_from_image(image_path):
+    image = Image.open(image_path)
+    return pytesseract.image_to_string(image, lang='ron')
+
+def create_docx_document(texts, output_file):
+    doc = Document()
+    for text in texts:
+        if text.strip():
+            doc.add_paragraph(text.strip())
+        doc.add_page_break()
+    doc.save(output_file)
+
+if __name__ == "__main__":
+    images = get_image_files()
+    texts = []
+
+    for image in images:
+        text = get_text_from_image(image)
+        texts.append(text)
+
+    output_file = "output.docx"
+    create_docx_document(texts, output_file)
+    print(f"Text from images saved to {output_file}")
